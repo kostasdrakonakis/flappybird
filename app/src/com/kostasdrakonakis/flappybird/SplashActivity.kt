772ba9dc569c13519820b55bc106d3dfa11f4f69
Konstantinos Drakonakis <kostasdrakonakis13@gmail.com>
2023-04-14 08:14:49 +0200

    Update project to Java 16

--- app/src/com/kostasdrakonakis/flappybird/SplashActivity.kt
+++ app/src/com/kostasdrakonakis/flappybird/SplashActivity.kt
@@ -15,17 +15,19 @@
  */
 package com.kostasdrakonakis.flappybird
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
 import com.github.kostasdrakonakis.androidnavigator.IntentNavigator
-import kotlinx.android.synthetic.main.activity_splash.*
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        playButton.setOnClickListener {
+        findViewById<Button>(R.id.playButton).setOnClickListener {
             IntentNavigator.startMainActivity(this)
         }
     }
